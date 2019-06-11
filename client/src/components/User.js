c870972cdee4e308d586be99eb3c64db05c62687
averastlast <carapowpowell@gmail.com>
2019-06-11 15:41:30 -0400

    single user com work

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,12 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 class User extends Component {
 
     state = {
-            user: {},
-            fav_team: [],
-            rival_team: []
+        userInfo: {
+            user: {
+                _id: '',
+                user_name: '',
+                email: '',
+                password: ''
+            },
+            fav_team: {
+                _id: '',
+                name: '',
+                rival_team: {
+                    _id: '',
+                    name: ''
+                }
+            }
+        },
+        redirectToHome: false,
+        isEditFormDisplayed: false
     }
 
     componentDidMount() {
@@ -25,14 +40,54 @@ class User extends Component {
         }
         catch (error) {
             console.log(error)
-            this.setState({error: error.message})
+            this.setState({ error: error.message })
         }
+    }
+
+    deleteUser = () => {
+        axios.delete(`/api/v1/users/${this.props.match.params.id}`).then(res => {
+            this.setState({ redirectToHome: true })
+        })
+    }
+
+    toggleEditForm = () => {
+        this.setState((state, props) => {
+            return { isEditFormDisplayed: !state.isEditFormDisplayed }
+        })
+    }
+
+    //this only changes user info, does not go to team selections
+
+    handleChange = (e) => {
+        const cloneUser = { ...this.state.userInfo }
+
+        let inside = cloneUser.user
+
+        inside[e.target.name] = e.target.value
+
+        cloneUser.user = inside
+
+        this.setState({ userInfo: cloneUser })
+    }
+
+    updateUser = (e) => {
+        e.preventDefault()
+        axios
+            .put(`/api/v1/users/${this.props.match.params.id}`, {
+                user_name: this.state.userInfo.user.user_name,
+                email: this.state.userInfo.user.email,
+                password: this.state.userInfo.user.password
+            })
+            .then(() => {
+                this.setState({ isEditFormDisplayed: false })
+                this.getUser()
+            })
     }
 
     render() {
         return (
             <div>
-                
+                {/* Is this mapping right? */}
                 <h1>{this.state.user.user_name}</h1>
                 {this.state.fav_teams.map(fav_team => (
                     <div key={fav_team.id}>
@@ -44,6 +99,64 @@ class User extends Component {
                         <h4>{rival_team.name}</h4>
                     </div>
                 ))}
+
+
+
+
+                <button class='button' onClick={this.toggleEditForm}>Edit</button>
+                {
+                    this.state.isEditFormDisplayed
+                        ? <form onSubmit={this.updateUser}>
+                            <p class='userform'>Edit User Form:</p>
+                            <div>
+                                <label htmlFor="user_name">Name:</label>
+                                <input
+                                    id="user_name"
+                                    type="text"
+                                    name="user_name"
+                                    onChange={this.handleChange}
+                                    value={this.state.userInfo.user.user_name}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email:</label>
+                                <textarea
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    onChange={this.handleChange}
+                                    value={this.state.emailInfo.email.email}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password:</label>
+                                <textarea
+                                    id="password"
+                                    type="number"
+                                    name="password"
+                                    onChange={this.handleChange}
+                                    value={this.state.userInfo.user.password}
+                                />
+                            </div>
+                            <button class='button'>Update</button>
+                        </form>
+                        : <div class='editbox'>
+                            <p class='userform'>Edit this user:</p>
+                            <p>
+                                Name: {this.state.userInfo.user.user_name}
+                            </p>
+                            <p>
+                                Email: {this.state.userInfo.user.email}
+                            </p>
+                            <p>
+                                Password: {this.state.userInfo.user.password}
+                            </p>
+                            <button class='deleteButton' onClick={this.deleteUser}>Delete</button>
+                        </div>
+                }
+
+
+
             </div>
         );
     }
