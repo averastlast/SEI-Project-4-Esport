fde6f7f9982d31d4b4a60663c4a61454b3e26628
averastlast <carapowpowell@gmail.com>
2019-06-16 21:25:54 -0400

    increased team pool

--- teamfav_app/pandaAPI.py
+++ teamfav_app/pandaAPI.py
@@ -7,8 +7,10 @@ headers = {
 }
 
 # r = requests.get('https://api.pandascore.co/ow/teams?token=' + key)
-
-r = requests.get('https://api.pandascore.co/ow/teams?token=' + key, headers=headers)
+# increased page size to 100, need to figure out how to get OWL teams only, 
+# API PS does not have any flags to determine if the team is National, OWL, Contenders, etc.
+# basically different divisions. All the teams are mixed.
+r = requests.get('https://api.pandascore.co/ow/teams?page[size]=100&token=' + key, headers=headers)
 
 pandaTeams = r.json()
 
